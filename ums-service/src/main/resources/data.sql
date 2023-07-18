5f15852083d658a70c9ffd10c5cc44a435950b27
DanielGraham123 <dg.danielboaz@yahoo.com>
2023-07-18 09:00:28 +0100

    postgres auth issue

--- ums-service/src/main/resources/data.sql
+++ ums-service/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 -- noinspection SqlDialectInspectionForFile
+ALTER USER postgres WITH PASSWORD 'root';
 
-CREATE DATABASE IF NOT EXISTS `ums` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE DATABASE IF NOT EXISTS `ums`; /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
